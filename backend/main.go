1fed550f47f392ac798d68eb4e9b33da1bef6008
Younes Outerbah <younesouterbah1@gmail.com>
2024-04-24 17:51:39 +0100

    connect to mysql database and perform crud operation

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -11,26 +14,51 @@ type Todo struct {
 	ID 		int 	`json:"id"`
 	Title 	string 	`json:"title"`
 	Done 	bool 	`json:"done"`
-	Body 	string 	`json:"body"`
 }
 
-func main() {
-	app := fiber.New()
+var db *sql.DB
 
+func databaseConnection(){
+	dsn := "root:eunice99x@tcp(localhost:3306)/todos?parseTime=true"
+
+	var err error
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Error opening database connection: %v", err)
+	}
+
+	// Check if the connection is successful
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	fmt.Println("Connected to MySQL database successfully")
+}
+
+func setupRoutes(app *fiber.App){
 	tasks := []Todo{}
 
-	app.Use(cors.New())
-
 	app.Post("/api/add", func(c *fiber.Ctx) error {
-		task := &Todo{}
+		// task := &Todo{}
+		task := new(Todo)
 
 		if err := c.BodyParser(task); err != nil {
 			return err
 		}
 
-		task.ID = len(tasks) + 1
+		result, err := db.Exec("INSERT INTO task (title, done) VALUES (?, ?)", task.Title, task.Done)
+		if err != nil {
+			return err
+		}
 
-		tasks = append(tasks, *task)
+		// Get the ID of the newly inserted task
+		id, err := result.LastInsertId()
+		if err != nil {
+			return err
+		}
+
+		task.ID = int(id)
 
 		return c.JSON(task)
 	})
@@ -52,9 +80,51 @@ func main() {
 		return c.JSON(tasks)
 	})
 
-	app.Get("/api/all", func(c *fiber.Ctx) error {
-		return c.JSON(tasks)
+	app.Delete("/api/delete/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+
+		_, err = db.Exec("DELETE FROM task WHERE id = ?", id)
+		if err != nil {
+			return err
+		}
+
+		return c.SendString("Task deleted successfully")
+
 	})
+
+	app.Get("/api/all", func(c *fiber.Ctx) error {
+		rows, err := db.Query("SELECT id, title, done FROM task")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	var tasks []Todo
+	for rows.Next() {
+		var task Todo
+		if err := rows.Scan(&task.ID, &task.Title, &task.Done); err != nil {
+			return err
+		}
+		tasks = append(tasks, task)
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	return c.JSON(tasks)	
+	})
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	databaseConnection()
+	setupRoutes(app)
 
 	log.Fatal(app.Listen(":8080"))
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>([]);
+  interface taskObj {
+    id: number;
+    title: string;
+    done: boolean;
+  }
+
+  const [tasks, setTasks] = useState<taskObj[]>([]);
   const [task, setTask] = useState<string>("");
 
+  async function getTasks() {
+    try {
+      const res = await fetch("http://localhost:8080/api/all");
+      const data = await res.json();
+      // console.log(data);
+      if (data.length > 0) {
+        setTasks(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const addTask = async () => {
-    setTask("");
-    setTasks([...tasks, task]);
-
-    const info = {
+    const newTask: taskObj = {
+      id: tasks.length + 1,
       title: task,
-      body: task,
+      done: false,
     };
-
+    setTask("");
     try {
       const res = await fetch("http://localhost:8080/api/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(info),
+        body: JSON.stringify(newTask),
       });
       const data = await res.json();
-      console.log(data);
+      if (data) {
+        setTasks([...tasks, newTask]);
+      }
     } catch (error) {
       console.log(error);
     }
   };
+
+  const deleteTask = async (id: number) => {
+    try {
+      const res = await fetch(`http://localhost:8080/api/delete/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      getTasks();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, [deleteTask]);
+
   return (
     <div className="bg-[#333] h-screen">
       <div className="container py-4 size-full">
@@ -43,9 +80,13 @@ function App() {
             className="bg-green-600 text-white p-2 rounded cursor-pointer"
             onClick={addTask}
           />
-          {tasks.map((TASK, id) => (
-            <div key={id} className="bg-black text-white mb-2 p-2 rounded">
-              {TASK}
+          {tasks?.map((task) => (
+            <div
+              key={task.id}
+              onClick={() => deleteTask(task.id)}
+              className="bg-black text-white mb-2 p-2 rounded"
+            >
+              {task.title}
             </div>
           ))}
         </div>
